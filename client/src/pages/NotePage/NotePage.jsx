d49f639fac4524b4af864a8f8af2579a81f17951
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2022-12-13 21:42:41 -0500

    Fixed certain naming conventions (#20)
    * Fixed certain naming and routing errors:
    - The reference to sidebar was mistyped due to the recent merge. Route is now working as expected.
    - Home and Profile were the pages with the wrong references.
    - Removed old junk files.
    - Other small fixes.
    
    * Fixed some of the naming conventions in the folders to avoid confusion.

--- client/src/pages/NotePage/NotePage.jsx
+++ client/src/pages/NotePage/NotePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../../components/Sidebar/Sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import NoteSpace from "../../components/Notespace";
 
